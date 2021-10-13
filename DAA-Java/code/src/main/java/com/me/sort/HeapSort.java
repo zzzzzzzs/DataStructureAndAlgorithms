9d2590750ebc993b4c9bc1f303570cc67efcc6df
zzzzzzzs <1443539042@qq.com>
2021-10-13 09:25:23 +0800

    Update HeapSort.java

--- DAA-Java/code/src/main/java/com/me/sort/HeapSort.java
+++ DAA-Java/code/src/main/java/com/me/sort/HeapSort.java
@@ -1,5 +1,7 @@
 package com.me.sort;
 
+import java.util.Arrays;
+
 /**
  * @author zs
  * @date 2021/10/12.
@@ -19,7 +21,7 @@ public class HeapSort {
 //        for (int i = 0; i < arr.length; i++) { // O(N)
 //            heapInsert(arr, i); // O(logN)
 //        }
-        for (int i = arr.length - 1; i >= 0 ; i--) { // O(N) 复杂度没有变化，只是第一步变快了
+        for (int i = arr.length - 1; i >= 0; i--) { // O(N) 复杂度没有变化，只是第一步变快了
             heapify(arr, i, arr.length);
         }
 
@@ -45,8 +47,7 @@ public class HeapSort {
     // 某个数在index位置，能否往下移动
     // heapSize管着是否越界以及堆的大小
     public static void heapify(int[] arr, int index, int heapSize) {
-        int left = index << 1 + 1; // 左孩子的下标
-
+        int left = (index << 1) + 1; // 左孩子的下标
         while (left < heapSize) { // 下方还有孩子的时候
             // 两个孩子中，谁的值大，把下标给largest
             int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
@@ -55,15 +56,87 @@ public class HeapSort {
             if (largest == index) break;
             swap(arr, largest, index);
             index = largest;
-            left = index << 1 + 1;
+            left = (index << 1) + 1;
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {1, 3, 2, 5, 4, 6};
-        heapInsert(arr, 1);
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
         }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test 对数器
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            heapSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        heapSort(arr);
+        printArray(arr);
     }
 }
