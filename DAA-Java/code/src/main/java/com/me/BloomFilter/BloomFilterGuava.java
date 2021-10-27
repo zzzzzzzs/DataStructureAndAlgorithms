24795916dc1725ee95fd62fef063bd04d28d14ba
zzzzzzzs <1443539042@qq.com>
2021-10-27 22:27:56 +0800

    update

--- DAA-Java/code/src/main/java/com/me/BloomFilter/BloomFilterGuava.java
+++ DAA-Java/code/src/main/java/com/me/BloomFilter/BloomFilterGuava.java
@@ -5,9 +5,9 @@ import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnels;
 
 public class BloomFilterGuava {
-    private static int size = 1000000;//预计要插入多少数据
+    private static int size = 100000000;//预计要插入多少数据
 
-    private static double fpp = 0.01;//期望的误判率
+    private static double fpp = 0.0001;//期望的误判率
 
     private static BloomFilter<Integer> bloomFilter = BloomFilter.create(Funnels.integerFunnel(), size, fpp);
 
---
+++ DAA-Java/code/src/main/java/com/me/Islands.java
@@ -0,0 +1,58 @@
+package com.me;
+
+/**
+ * @author zs
+ * @date 2021/10/27
+ */
+public class Islands {
+    public static int countIslands(int[][] m) {
+        if (m == null || m[0] == null) {
+            return 0;
+        }
+        int N = m.length;
+        int M = m[0].length;
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (m[i][j] == 1) {
+                    res++;
+                    infect(m, i, j, N, M);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void infect(int[][] m, int i, int j, int N, int M) {
+        if (i < 0 || i >= N || j < 0 || j >= M || m[i][j] != 1) {
+            return;
+        }
+        // i，j没越界，并且当前位置值是1
+        m[i][j] = 2; // 一上来就把当前位置改成2，然后上下左右递归进行
+        infect(m, i + 1, j, N, M);
+        infect(m, i - 1, j, N, M);
+        infect(m, i, j + 1, N, M);
+        infect(m, i, j - 1, N, M);
+    }
+
+    public static void main(String[] args) {
+        int[][] m1 = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 0, 1, 1, 1, 0},
+                {0, 1, 1, 1, 0, 0, 0, 1, 0},
+                {0, 1, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},};
+        System.out.println(countIslands(m1));
+
+        int[][] m2 = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 1, 1, 0, 0, 0, 1, 0},
+                {0, 1, 1, 0, 0, 0, 1, 1, 0},
+                {0, 0, 0, 0, 0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},};
+        System.out.println(countIslands(m2));
+
+    }
+}
