6a4d6a3b6d31a6ae9cd133680c210488e0bc615c
zzzzzzzs <1443539042@qq.com>
2021-11-02 22:33:50 +0800

    Create Test1.java

---
+++ DAA-Java/code/src/test/java/Test1.java
@@ -0,0 +1,12 @@
+/**
+ * @author zs
+ * @date 2021/11/2
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        int L = 0;
+        int R = 2;
+        int mid = L + ((R - L) >> 1);
+        System.out.println(mid);
+    }
+}
